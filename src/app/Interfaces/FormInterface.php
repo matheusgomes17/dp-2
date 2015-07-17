808926e84ece5baccde3d3739685efba7d38d3b4
Matheus Gomes <zuza-cs@hotmail.com>
2015-07-17 01:40:02 -0300

    Corrigindo interface

--- src/app/Interfaces/FormInterface.php
+++ src/app/Interfaces/FormInterface.php
@@ -8,5 +8,7 @@ interface FormInterface
 {	
 	public function addElement(\app\Interfaces\FormFieldInterface $field);
 
+	public function createField($field, $name, $value, $placeholder, $cols, $rows);
+
 	public function render();
 }
